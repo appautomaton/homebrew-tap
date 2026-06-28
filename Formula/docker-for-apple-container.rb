80e48524abe78f5e55ac293c6959bed02e56a88a
BP <11394934+benjipeng@users.noreply.github.com>
2026-06-27 23:25:43 -0400

    Bump to v0.1.1

--- Formula/docker-for-apple-container.rb
+++ Formula/docker-for-apple-container.rb
@@ -1,8 +1,8 @@
 class DockerForAppleContainer < Formula
   desc "Docker CLI compatibility shim over Apple's container CLI"
   homepage "https://github.com/appautomaton/docker-for-apple-container"
-  url "https://github.com/appautomaton/docker-for-apple-container/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "47ed0a0af4334b81bc519d301b98443b41330991526578c2a1b73018ab7880a7"
+  url "https://github.com/appautomaton/docker-for-apple-container/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "95898d378b3b670b7b0b449edb49d699b6eead93e338cb8be7b3e1d0001ef2e7"
   license "MIT"
 
   def install
